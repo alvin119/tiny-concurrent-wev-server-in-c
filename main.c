d792cba058e494e11ec53245475f99c0b1ce4f13
alvinwu <alvinwu119@gmail.com>
2026-05-21 11:16:40 +0800

    add HTTP parser and use fork() to achieve multi-process

--- main.c
+++ main.c
@@ -7,6 +7,10 @@ server: getaddrinfo => socket => bind => listen => accept
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/sendfile.h>
+#include <fcntl.h>
+#include <signal.h>
 
 #include <arpa/inet.h> // htonl
 #include <sys/socket.h> // socket(), bind(), listne()...
@@ -19,6 +23,7 @@ server: getaddrinfo => socket => bind => listen => accept
 // int sscanf(const char *str, const char *format, ...);
 // return 成功放入幾個
 
+// 拿到 method ＆ path
 // return 0 表示成功，return -1 表示失敗
 int parse_request(const char *buf, char *method, char *path){
     if(sscanf(buf, "%15s %1023s", method, path) != 2){
@@ -40,6 +45,88 @@ void send_response(int fd, int status_code, const char *status_text, const char 
     write(fd, header, header_len);
     write(fd, body, body_len);
 }
+
+const char *get_mime_type(const char *path){
+    // file extension 副檔名
+    const char *ext = strrchr(path, '.');
+    if(ext == NULL) return "application/octet-stream";
+    if (strcmp(ext, ".html") == 0 || strcmp(ext, ".htm") == 0) return "text/html";
+    if (strcmp(ext, ".jpg")  == 0 || strcmp(ext, ".jpeg") == 0) return "image/jpeg";
+    if (strcmp(ext, ".png")  == 0) return "image/png";
+    if (strcmp(ext, ".css")  == 0) return "text/css";
+    if (strcmp(ext, ".js")   == 0) return "application/javascript";
+    if (strcmp(ext, ".txt")  == 0) return "text/plain";
+    return "application/octet-stream";  
+}
+
+void uri_to_path(const char* uri, char *path, size_t size){
+    // 如果是根目錄
+    if(strcmp(uri, "/") == 0){
+        snprintf(path, size, "./www/index.html");
+        return;
+    }
+    // 防止 path traversal 攻擊
+    if(strstr(uri, "..") != NULL){
+        path[0] = '\0';
+        return;
+    }
+    // 其他就前面加上 /www
+    snprintf(path, size, "./www%s", uri);
+}
+
+// 將 uri 轉成 server 的路徑
+// 分析路徑再拿到　MIME type，寫好 header
+// 將寫好的　header 送出後再送出 body
+void serve_static_file(const char *uri, int conn_fd){
+    // uri 轉成 server 中的 disk route
+    char path[1024];
+    uri_to_path(uri, path, sizeof(path));
+    if(path[0] == '\0'){
+        send_response(conn_fd, 400, "Bad Request", "text/plain", "400 Bad Request");
+    }
+    
+    // 確認 path 有沒有檔案，是不是 dir
+    struct stat st;
+    if (stat(path, &st) < 0 || !S_ISREG(st.st_mode)) {
+        send_response(conn_fd, 404, "Not Found", "text/plain", "404 Not Found");
+        return;
+    }
+
+    // 開檔
+    int file_fd = open(path, O_RDONLY);
+    if(file_fd < 0){
+        send_response(conn_fd, 500, "Internal Server Error",
+                  "text/plain", "500 Internal Server Error");
+        return;
+    }
+
+    // 取得 mime Type
+    const char *mime = get_mime_type(path);
+
+    // 設定 header
+    char header[1024];
+    int header_len = snprintf(header, sizeof(header), 
+        "HTTP/1.0 200 OK\r\n"
+        "Content-Type: %s\r\n"
+        "Content-Length: %ld\r\n"
+        "\r\n",
+        mime, (long)st.st_size
+    );
+
+    write(conn_fd, header, header_len);
+
+    // 送 body（zero-copy）
+    sendfile(conn_fd, file_fd, NULL, st.st_size);
+
+    close(file_fd);
+}
+
+void sigchld_handler(int sig) {
+    // 用 waitpid 回收所有 zombie
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+void 
 
 int main(){
     printf("Starting server on port: %d\n", PORT);
@@ -70,6 +157,9 @@ int main(){
         perror("listen");
         exit(1);
     }
+
+    // signal: 傳送通知給 process，告訴 os handler 在哪
+    signal(SIGCHLD, sigchld_handler);
     // accept
     while(1){
         struct sockaddr_in client_addr;
@@ -105,7 +195,7 @@ int main(){
             close(conn_fd);
             continue;
         }
-
+        // 只先處理 GET Request
         if (strcmp(method, "GET") != 0) {
             send_response(conn_fd, 405, "Method Not Allowed",
                         "text/plain", "405 Method Not Allowed");
@@ -113,12 +203,27 @@ int main(){
             continue;
         }
 
-        send_response(conn_fd, 200, "OK", "text/html", "<h1>Hello from Tiny Server!</h1>");
-
-
-        close(conn_fd);
+        pid_t pid = fork();
+        if(pid == 0){
+            // child process
+            close(server_fd);
+            serve_static_file(path, conn_fd);
+            close(conn_fd);
+            exit(0);
+        }else if(pid > 0){
+            // parent process
+            close(conn_fd);
+            continue;
+        }else{
+            // 小於 0，系統沒資源
+            perror("fork");
+            close(conn_fd);
+        }
+        
     }
 
     
     return 0;
 }
+
+
